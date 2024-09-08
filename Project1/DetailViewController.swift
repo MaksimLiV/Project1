3808c1df06255b257cd12f2960df76e77cd05ab5
Maksim Li <maximlee.ml@gmail.com>
2024-09-08 18:28:46 +0300

    Project upload

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Maksim Li on 05/09/2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+
+        if let imageToLoad = selectedImage, let image = UIImage(named: imageToLoad) {
+            imageView.image = image
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let navController = navigationController {
+            navController.hidesBarsOnTap = true
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let navController = navigationController {
+            navController.hidesBarsOnTap = false
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
